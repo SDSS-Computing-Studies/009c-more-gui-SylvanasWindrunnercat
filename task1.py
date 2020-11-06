66dd765f4904a5da4dedfe7dd308ae09fe30b261
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-06 11:28:25 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,6 +9,8 @@ window = tk.Tk()
 window.title("Triangle Calculater")
 window.geometry("450x150")
 
+d = ""
+
 def Triangle():
     a = e1.get()
     a = float(a)
@@ -26,13 +28,13 @@ def Triangle():
         s = (a + b + c) / 2
         d = math.sqrt(s (s - a) * (s - b) * (s - c))
     elif a == 0 or b == 0 or c == 0:
-        answer = "Erorr,lack information"
+        d = "Erorr Message"
     else:
         b = int(b)
         h = int(h)
         d = b * h / 2
     a_entry.delete(0,END)
-    a_entry.insert(0,answer)
+    a_entry.insert(0,d)
 
 l0 = tk.Label(window,text= "If you don't know the length,please enter 0 to instead")
 l1 = tk.Label(window,text = "length of side1")
@@ -44,6 +46,8 @@ e3 = tk.Entry(window,width = 10)
 l4 = tk.Label(window,text = "length of high")
 e4 = tk.Entry(window,width = 10)
 b1 = tk.Button(window,text = "Calculate",command = Triangle)
+l5 = tk.Label(window,text = "The output is: ")
+e5 = tk.Entry(window,width = 10)
 
 l0.grid(row = 1,column = 1)
 l1.grid(row = 2,column = 1)
@@ -55,5 +59,7 @@ e3.grid(row = 4,column = 2)
 l4.grid(row = 5,column = 1)
 e4.grid(row = 5,column = 2)
 b1.grid(row = 1,column = 3)
+l5.grid(row = 6,column = 1)
+e5.grid(row = 6,column = 2)
 
 window.mainloop()
